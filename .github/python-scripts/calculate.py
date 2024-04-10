cee3da08576b67af4da94ac4ddce9f17d0ec3b55
Jaya Bhattacharya <jaya.x.bhattacharya@kp.org>
2024-04-10 13:34:10 -0700

    added another python script with first class func - fixed

--- .github/python-scripts/calculate.py
+++ .github/python-scripts/calculate.py
@@ -17,9 +17,12 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-def calculate(n1, n2, func):
-  return func(n1, n2)
+func_list = {"add": add, "subtract": subtract, "multiply": multiply, "divide": divide}
 
-# print(f"{input_FIRST_num} {func_needed} {input_SECOND_num} = {calculate(input_FIRST_num, input_SECOND_num, func_needed)}")
-result = calculate(input_FIRST_num, input_SECOND_num, func_needed)
-print(f"{input_FIRST_num} {func_needed} {input_SECOND_num} = {result}")
+def calculate(n1, n2, func):
+  for key in func_list:
+      if key == func:
+        return func_list[key](n1, n2)
+
+
+print(f"{input_FIRST_num} {func_needed} {input_SECOND_num} = {calculate(input_FIRST_num, input_SECOND_num, func_needed)}")
