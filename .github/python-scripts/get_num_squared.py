706cd690d08d945b371123d5b17d9faa525a0ec4
Jaya Bhattacharya <jaya.x.bhattacharya@kp.org>
2024-04-10 11:11:52 -0700

    call python script

--- .github/python-scripts/get_num_squared.py
+++ .github/python-scripts/get_num_squared.py
@@ -1,7 +1,7 @@
 print("Hello world!")
 import os
 
-input_num = os.environ.get("GITHUB_ENV")
+input_num = os.environ.get("INPUT_NUM")
 print(f"Input number provided ${input_num}")
 def square(num):
     return num * num
